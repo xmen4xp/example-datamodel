da29268eac81080f451995b2c6b0201c110cedc2
xmen4xp <dbakiara@danielga-mobl.amr.corp.intel.com>
2024-06-19 13:47:29 -0700

    Add rest api for category

--- datamodel/category/category.go
+++ datamodel/category/category.go
@@ -5,6 +5,20 @@ import (
 	"github.com/vmware-tanzu/graph-framework-for-microservices/nexus/nexus"
 )
 
+var CategoryRestAPISpec = nexus.RestAPISpec{
+	Uris: []nexus.RestURIs{
+		{
+			Uri:     "/category/{category.Category}",
+			Methods: nexus.DefaultHTTPMethodsResponses,
+		},
+		{
+			Uri:     "/categories",
+			Methods: nexus.HTTPListResponse,
+		},
+	},
+}
+
+// nexus-rest-api-gen:CategoryRestAPISpec
 type Category struct {
 	nexus.Node
 
